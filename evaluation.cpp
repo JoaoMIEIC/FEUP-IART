0afbd43c3b2ad0ad2cfcf085586e39b7885a0980
andrelds11 <andreldsantos11@gmail.com>
2022-04-13 17:32:38 +0100

    evaluation functions updated

--- evaluation.cpp
+++ evaluation.cpp
@@ -4,8 +4,31 @@
 int evaluateFinishedWithTime(vector<Car> &cars, int duration){
     int score = 0;
     for (auto & car : cars) {
-        if (car.getPath().empty()) score += 1000;
-        score += (duration - (car.getTotalTimeWaiting() + car.getTotalTravelDist()));
+        if (car.getPath().empty()) {
+            score += 100;
+            score += 10*(duration - (car.getTotalTimeWaiting() + car.getTotalTravelDist()));
+        }
     }
     return score;
+}
+
+int evaluateWithPercentage(vector<Car> &cars, int duration){
+    int score = 0;
+    for (auto & car : cars) {
+        if (car.getPath().empty()) score += 100;
+        else score += getPercentageDone(&car);
+    }
+    return score;
+}
+
+int getPercentageDone(Car* car){
+    float distanceDone = car->getTotalTravelDist();
+    float distanceLeft = car->getDistTilIntersection();
+
+    car->getPath().pop();
+    while(!car->getPath().empty()){
+        distanceLeft += car->getPath().front()->getDuration();
+    }
+
+    return 100 * (distanceDone / (distanceDone + distanceLeft));
 }
--- evaluation.h
+++ evaluation.h
@@ -3,3 +3,9 @@
 #include "graph.h"
 
 int evaluateFinishedWithTime(vector<Car> &cars, int duration);
+
+int evaluateFinishedWithPercentage(vector<Car> &cars, int duration);
+
+int getPercentageDone(Car* car);
+
+
